8bce57eaa2c4fcfb3b9d0a2a519cb33f4787044c
honguyen <honguyen.dev2k2@gmail.com>
2023-08-17 15:39:50 +0700

    fix deploy zalo

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,7 @@
-import React, { Suspense, useEffect, useRef } from "react";
-import is from "is_js";
-// import WebRoot from "./web/Root";
-// import { CloseButton } from 'components/common/Toast';
-// import SettingsToggle from 'components/settings-panel/SettingsToggle';
-// import SettingsPanel from 'components/settings-panel/SettingsPanel';
-<<<<<<< Updated upstream
-import 'react-datepicker/dist/react-datepicker.css';
-import 'react-toastify/dist/ReactToastify.min.css';
-import { RecoilRoot } from 'recoil';
-import Main from './Main';
-=======
-
-import Main from "./Main";
+import React, { Suspense, useEffect, useRef, useState } from "react";
 import { Spinner } from "react-bootstrap";
+import is from "is_js";
+import Main from "./Main";
 
 const WebRoot = React.lazy(() => import("./web/Root"));
 const ZaloRoot = React.lazy(() => import("./zalo/Root"));
@@ -36,23 +25,22 @@ const LoadingComponent = () => {
     </div>
   );
 };
->>>>>>> Stashed changes
 
 const App = () => {
+  const [platform, setPlatform] = useState("zalo");
   const HTMLClassList = document.getElementsByTagName("html")[0].classList;
 
-  const platformIsZalo = useRef(false);
   useEffect(() => {
-    const checkZaloPlatform = async () => {
+    const checkPlatform = async () => {
       const port = await location.port; // zmp start chạy trên port 3000 -> zaloweb: 2999
       const isZalo = (await navigator.userAgent.indexOf("Zalo")) !== -1; // chạy trên thiết bị thật
-      if (isZalo || port == "2999") {
-        // phát triển zalo mini app
-        platformIsZalo.current = true;
+      if (!isZalo && port !== "2999") {
+        // phát triển web
+        setPlatform("web")
       }
     };
 
-    checkZaloPlatform();
+    checkPlatform();
   }, []);
 
   useEffect(() => {
@@ -69,14 +57,7 @@ const App = () => {
       HTMLClassList.add("safari");
     }
   }, [HTMLClassList]);
-<<<<<<< Updated upstream
 
-
-=======
-  // const { isLoaded } = useToggleStyle(false, false, null);
-
-  // console.log(isLoaded);
->>>>>>> Stashed changes
   /* useEffect(() => {
     if (navbarPosition === 'double-top') {
       HTMLClassList.add('double-top-nav-layout');
@@ -85,29 +66,11 @@ const App = () => {
   }, [navbarPosition]); */
 
   return (
-<<<<<<< Updated upstream
-    <RecoilRoot>
-    <Main>
-    <Router basename={process.env.NODE_ENV === "production" ? `/zapps/${window.APP_ID}` : ""}>
-      <DigiBirdRoutes />
-      {/* <SettingsToggle />
-      <SettingsPanel />
-      <ToastContainer
-        closeButton={CloseButton}
-        icon={false}
-        position={toast.POSITION.BOTTOM_LEFT}
-      /> */}
-    </Router>
-    </Main>
-    </RecoilRoot>
-=======
     <Suspense fallback={<LoadingComponent />}>
       <Main>
-        {platformIsZalo ? <ZaloRoot /> : <WebRoot />}
-        {/* <ZaloRoot />  */}
+        {platform === "zalo" ? <ZaloRoot /> : <WebRoot />}
       </Main>
     </Suspense>
->>>>>>> Stashed changes
   );
 };
 
