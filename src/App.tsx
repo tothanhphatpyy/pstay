7259643b68db4ecd93268c8aa4ddb75e3b2c7314
honguyen <honguyen.dev2k2@gmail.com>
2023-08-18 14:09:52 +0700

    update recoil-logger

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import is from "is_js";
 import Main from "./Main";
 import WebRoot from "./web/Root";
-import ZaloRoot from "./zalo/Root"  
-import useToggleStylesheet from "./hooks/useToggleStyle";
+import ZaloRoot from "./zalo/Root";
 
 const App = () => {
   const [platform, setPlatform] = useState("zalo");
+  const [loading, setloading] = useState(true);
   const HTMLClassList = document.getElementsByTagName("html")[0].classList;
 
   useEffect(() => {
@@ -15,7 +15,8 @@ const App = () => {
       const isZalo = (await navigator.userAgent.indexOf("Zalo")) !== -1; // chạy trên thiết bị thật
       if (!isZalo && port !== "2999") {
         // phát triển web
-        setPlatform("web")
+        setPlatform("web");
+        setloading(false);
       }
     };
 
@@ -43,11 +44,23 @@ const App = () => {
     }
     return () => HTMLClassList.remove('double-top-nav-layout');
   }, [navbarPosition]); */
-  return (
-      <Main>
-        {platform === "zalo" ? <ZaloRoot /> : <WebRoot />}
-      </Main>
-  );
+
+  if (loading) {
+    return (
+      <div
+        style={{
+          position: "fixed",
+          top: 0,
+          right: 0,
+          bottom: 0,
+          left: 0,
+          backgroundColor: "rgba(204,204,204,0.4)",
+        }}
+      />
+    );
+  }
+
+  return <Main>{platform === "zalo" ? <ZaloRoot /> : <WebRoot />}</Main>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,16 @@ declare const window: any;
 
 import React from "react";
 import { createRoot } from "react-dom/client";
+import App from "./App";
+import appConfig from "../app-config.json";
+import { RecoilRoot } from "recoil";
+import RecoilLogger from "recoil-logger";
 
 import '@helpers/initFA';
 import 'react-datepicker/dist/react-datepicker.css';
 import 'react-toastify/dist/ReactToastify.min.css';
 import './assets/scss/theme.scss'
 import "../i18n-config";
-
-import App from "./App";
-import appConfig from "../app-config.json";
-import { RecoilRoot } from "recoil";
 
 if (!window.APP_CONFIG) {
   window.APP_CONFIG = appConfig;
@@ -24,6 +24,7 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <RecoilRoot>
+      <RecoilLogger />
       <App />
     </RecoilRoot>
   </React.StrictMode>
