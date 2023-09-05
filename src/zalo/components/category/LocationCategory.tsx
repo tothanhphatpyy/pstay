bab364698cee45172baf4989de590cd69397aa2e
phat dev <phat.tt@digibird.io>
2023-09-05 09:06:32 +0700

    add component Text

--- src/zalo/components/category/LocationCategory.tsx
+++ src/zalo/components/category/LocationCategory.tsx
@@ -2,31 +2,47 @@ import { useLocation } from "@atom/location/useLocation";
 import Avatar from "@shared/Avatar";
 import Flex from "@shared/Flex";
 import React from "react";
-import { Col } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 import "./styles.scss";
+import Text from "@shared/text/Text";
 
 const LocationCategory = () => {
   const { location } = useLocation();
   return (
-    // <div className="scrollbar ps-3 d-flex">
-    //     {location.map((item, index) => (
-    //       <Col className="mr-4" key={index}>
-    //         <Avatar src={item.image} className="img-category" />
-    //         <div className="text-5-medium">{item.name}</div>
-    //       </Col>
-    //     ))}
-    //   </div>
+   
     <div className="scrollbar px-3 d-flex">
       {location.map((item, index) => (
         <Col className={index === (location?.length -1) ? '' : 'mr-4'} key={index}>
           <div className="list-category">
             <Avatar src={item.image} className="img-category ms-2" />
-            <div className="text-5-medium">{item.name.length > 9 ? `${item.name.slice(0, 9)}..` : item.name}</div>
+            <Text className="text-5-medium mt-1">{item.name.length > 9 ? `${item.name.slice(0, 9)}..` : item.name}</Text>
           </div>
         </Col>
       ))}
     </div>
+/*     <Container>
+  <Row xs={2} md={4} lg={12} className="p-2">
+    <Col className="p-2 bg-300 border border-400">1 of 2</Col>
+    <Col className="p-2 bg-300 border border-400">2 of 2</Col>
+    <Col className="p-2 bg-300 border border-400">2 of 2</Col>
+    <Col className="p-2 bg-300 border border-400">2 of 2</Col>
+    <Col className="p-2 bg-300 border border-400">2 of 2</Col>
+    
+  </Row>
+  <Row xs={1} md={2} className="p-2">
+    <Col className="p-2 bg-300 border border-400">1 of 3</Col>
+    <Col className="p-2 bg-300 border border-400">2 of 3</Col>
+    <Col className="p-2 bg-300 border border-400">3 of 3</Col>
+  </Row>
+  <Row xs="auto" className="p-2">
+    <Col className="p-2 bg-300 border border-400">1 of 3</Col>
+    <Col className="p-2 bg-300 border border-400">2 of 3</Col>
+    <Col className="p-2 bg-300 border border-400">3 of 3</Col>
+  </Row>
+</Container> */
   );
 };
 
 export default LocationCategory;
+
+
--- src/zalo/pages/tab/Home.tsx
+++ src/zalo/pages/tab/Home.tsx
@@ -9,7 +9,7 @@ const Home = () => {
     <>
       <Flex alignItems={'center'} className='container'>
         <Image src={logo} height={60} width={65} className="object-fit-contain" />
-        <span className="heading-4-medium text-color-primary">PSTAY</span>
+        <span className="heading-4-medium text-color-primary text-primary-cl">PSTAY</span>
       </Flex>
       <LocationCategory />
     </>
